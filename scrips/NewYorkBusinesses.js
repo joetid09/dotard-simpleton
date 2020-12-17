82e7f616084c25a6fae2e45d90f312680f6c2f94
joseph.tidwell09@gmail.com <joseph.tidwell09@gmail.com>
2020-12-16 18:37:17 -0600

    filtered out the new your and Manufacturing companies

---
+++ scrips/NewYorkBusinesses.js
@@ -0,0 +1,19 @@
+import { useBusinesses } from "./BusinessProvider.js"
+import { Business } from "./Business.js"
+
+const contentTarget = document.querySelector(".businessList--newYork")
+
+export const newYorkBusinessList = () => {
+    debugger;
+    const businessList = useBusinesses();
+    const newYorkBusinesses = businessList.filter(business => {
+        if (business.addressStateCode == "NY") {
+            return true;
+        }
+    })
+
+    newYorkBusinesses.forEach(business => {
+        contentTarget.innerHTML += Business(business)
+    })
+
+}
